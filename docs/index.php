163ea7baa83ce0f45d8ca5286ae8f85df5088863
JT <certainthings@gmail.com>
2018-10-09 16:52:52 +1100

    init

---
+++ docs/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Welcome to Python Programming!</title>
+
+  <link rel="stylesheet" type="text/css" href="https://nagale.com/proxima-nova/fonts.css" />
+  <link rel="stylesheet" href="ga-title.css">
+
+
+  <style type="text/css">
+
+    .reveal a {
+      color: #fff;
+      text-decoration: underline;
+    }
+  </style>
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body class="reveal">
+  <p><img src="images/GA_Cog_Medium_White_RGB.png" /></p>
+
+  <h3>Welcome to Python Programming!</h3>
+  <p>Here are the links to your presentations.</p>
+  <br />
+  <p>Unit 1: Variables:</p>
+  <ul>
+    <li><a href="01-welcome.html" target="_blank">Lesson 1: Welcome</a></li>
+    <li><a href="02-variables.html" target="_blank">Lesson 2: Variables</a></li>
+    <li><a href="03-local-python.html" target="_blank">Lesson 3: Running Python on Your Computer</a></li>
+    <li><a href="04-unit-lab-1.html" target="_blank">Lesson 4: First Unit Lab</a></li>
+  </ul>
+  <br />
+  <p>Unit 2: Control Flow:</p>
+  <ul>
+    <li><a href="05-conditionals.html" target="_blank">Lesson 5: Conditionals</a></li>
+    <li><a href="06-lists.html" target="_blank">Lesson 6: Lists</a></li>
+    <li><a href="07-loops.html" target="_blank">Lesson 7: Loops</a></li>
+    <li><a href="08-functions.html" target="_blank">Lesson 8: Functions</a></li>
+    <li><a href="09-args.html" target="_blank">Lesson 9: Function Arguments</a></li>
+    <li><a href="10-unit-lab-2.html" target="_blank">Lesson 10: Second Unit Lab</a></li>
+  </ul>
+  <br />
+  <p>Unit 3: Object Oriented Programming:</p>
+  <ul>
+    <li><a href="11-dictionaries.html" target="_blank">Lesson 11: Dictionaries</a></li>
+    <li><a href="12-sets-tuples.html" target="_blank">Lesson 12: Sets and Tuples</a></li>
+    <li><a href="13-classes.html" target="_blank">Lesson 13: Classes</a></li>
+    <li><a href="14-inheritance.html" target="_blank">Lesson 14: Inheritance</a></li>
+    <li><a href="15-unit-lab-3.html" target="_blank">Lesson 15: Third Unit Lab</a></li>
+  </ul>
+  <p>Unit 4: Troubleshooting:</p>
+  <ul>
+    <li><a href="16-variable-scope.html" target="_blank">Lesson 16: Variable Scope</a></li>
+    <li><a href="17-intermediate-variables.html" target="_blank">Lesson 17: Intermediate Variables</a></li>
+    <li><a href="18-debugging.html" target="_blank">Lesson 18: Troubleshooting</a></li>
+    <li><a href="19-unit-lab-4.html" target="_blank">Lesson 19: Fourth Unit Lab</a></li>    
+
+  </ul>
+  <p>Unit 5: Intermediate Python:</p>
+  <ul>
+    <li><a href="20-inter-intro.html" target="_blank">Lesson 20: Review and Intro to Intermediate Python</a></li>    
+    <li><a href="21-scripting.html" target="_blank">Lesson 21: Scripting</a></li>
+    <li><a href="22-code-abstraction.html" target="_blank">Lesson 22: Code Abstraction</a></li>
+    <li><a href="23-modules.html" target="_blank">Lesson 23: Modules</a></li>
+    <li><a href="24-apis.html" target="_blank">Lesson 24: APIs</a></li>
+    <li><a href="25-unit-lab-5.html" target="_blank">Lesson 25: Fifth Unit Lab</a></li>
+
+  </ul>
+  <p>Unit 6 - Data Track: Pandas:</p>
+  <ul>
+    <li><a href="pandas-1-intro-ds.html" target="_blank">Lesson 1: Intro to DS</a></li>
+    <li><a href="pandas-2-basic-pandas.html" target="_blank">Lesson 2: Basic Pandas</a></li>
+    <li><a href="pandas-3-visualizations.html" target="_blank">Lesson 3: Data Visualization</a></li>
+    <li><i>Lesson 4: Plotting with Pandas is entirely in the Jupyter Notebook</i></li>
+    <li><a href="pandas-5-pandas-2.html" target="_blank">Lesson 5: Pandas II</a></li>
+    <li><a href="pandas-6-unit-lab.html" target="_blank">Lesson 6: Pandas Unit Lab</a></li>
+    <li><a href="pandas-7-next-steps.html" target="_blank">Lesson 7: Pandas Next Steps</a></li>
+  
+  </ul>
+  <p>Unit 6 - Web Dev Track: Flask:</p>
+  <ul>
+    <li><a href="flask-1-intro-web-dev.html" target="_blank">Lesson 1: Intro to Web Dev</a></li>
+    <li><a href="flask-2-intro-flask.html" target="_blank">Lesson 2: Intro to Flask</a></li>
+    <li><a href="flask-3-styling-flask.html" target="_blank">Lesson 3: Styling Flask</a></li>
+  </ul>
+  
+</body>
+</html>
